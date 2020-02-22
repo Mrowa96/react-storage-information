f670ff597f8a631b2a1fc156546de6831c6d5806
Mrowa96 <work@pawel-mrowiec.pl>
2020-02-22 23:27:41 +0100

    Cleanup update (#1)
    * Quite big update
    
    * Add new lines at the end of some files and remove entries from .npmignore
    
    * Add github publish workflow
    
    * Remove eslint-config-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,3 @@
 module.exports = {
-  extends: ['airbnb-base', 'plugin:prettier/recommended', 'prettier', 'prettier/standard'],
-  plugins: ['prettier'],
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    'import/no-extraneous-dependencies': 'off',
-  },
-  env: {
-    browser: false,
-    node: true,
-  },
+  extends: ['@mrowa96/eslint-config-react'],
 };
